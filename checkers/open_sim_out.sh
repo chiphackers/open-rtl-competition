65ddbc9352e35e1d54b4db7d94b8d766fde28e07
udara28 <udara28@live.com>
2018-04-07 17:47:46 -0400

    adding changes to open_sim

--- checkers/open_sim_out.sh
+++ checkers/open_sim_out.sh
@@ -13,8 +13,8 @@ if [ -z $1 ]; then
 fi
 
 ######### Goto Answer ###############
-TEST_DIR=$1
-cd $1
+PROB_DIR=$1
+TEST_FILE=$2
 
 ######### Sanity Work ###############
 if [ -f compile.log ]; then
@@ -23,15 +23,18 @@ fi
 
 ############# Complie  #############
 
-iverilog TEST testbench.v |& tee compile.log
+iverilog $TEST_FILE $PROB_DIR/testbench.v |& tee compile.log
 
 if [ "`cat compile.log`" != "" ]; then
     echo "[ERROR  ]: compilation failed"
     echo "----------[LOG]------------"
     cat compile.log
     echo "----------[END]------------"
+    rm compile.log
     exit 1
 fi
+
+rm compile.log
 
 ######### Sanity Work ###############
 if [ -f sim.out ]; then
@@ -43,8 +46,8 @@ vvp a.out > sim.out
 
 
 ######### Line count check #########
-line_count_sim=`cat sim.out | wc -l`
-line_count_sol=`cat SOLUTION | wc -l`
+line_count_sim="`cat sim.out | wc -l`"
+line_count_sol="`cat $PROB_DIR/SOLUTION | wc -l`"
 
 if [[ $line_count_sim != $line_count_sol ]]; then
     echo "[ERROR  ]: Output mismatch found. Do you have any \$display in your designs. Please remove them."
@@ -57,12 +60,19 @@ if [ -f diff.out ]; then
 fi
 
 ######### DIFF ######################
-diff --new-line-format="[ERROR  ]: %L" --old-line-format="" --unchanged-line-format="[PASS   ]: %L" SOLUTION sim.out |& tee diff.out
+diff --new-line-format="[ERROR  ]: %L" --old-line-format="" --unchanged-line-format="[PASS   ]: %L" $PROB_DIR/SOLUTION sim.out |& tee diff.out
 echo -e "\n[ TESTING DONE ]\n"
 
 grep -s -q '^\[ERROR  \]:' diff.out
+COMP_RESULT=$?
 
-if [[ $? == 0 ]]; then
+######### Clean up #################
+rm sim.out
+rm a.out
+
+rm diff.out
+
+if [[ $COMP_RESULT == 0 ]]; then
     echo "[ERROR  ]: Your design is not correct"
     exit 1
 else
